cb04dda94aa8a7a297698f2d44020747b9724c5a
vagrant <vagrant@localhost.localdomain>
2020-07-23 01:35:15 +0000

    “[変更なし]”

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,28 +1,17 @@
 class CartItemsController < ApplicationController
 	def index
-		@cart_items = Cart_item.all
 	end
+	# def create
+	# end
 
-	def create
-	end
+	# def update
+	# end
 
-	def update
+	# def destroy
+	# end
 
-	end
-
-	def destroy
-		cart_item = Cart_item.find(params[:id])
-        cart_item.destroy
-        redirect_to cart_items_path
-	end
-
-	def destroy_all
-	   @cart_items = current_cart
-       @cart_items.destroy
-       session[:cart_items_id] = nil
-       respond_to do |format|
-       format.html { redirect_to cart_items_path, notice: 'カートが空になりました。' }
-       format.json { head :no_content }
-    end
-	end
+	# private
+	# def cart_items_params
+	# 	params.require(:cart_items).permit(:totall_nunber)
+	# end
 end
