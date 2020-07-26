9f32b06f8827177e179417bc9b43d3f55fc308ad
vagrant <vagrant@localhost.localdomain>
2020-07-26 13:40:53 +0000

    shinoda

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order < ApplicationRecord
 
 has_many :order_product, dependent: :destroy
 belongs_to :member
-enum payment_method: { "銀行振り込み": false, "クレジットカード": true }
+enum payment_method: {'クレジットカード': 0, '銀行振込': 1 }
+enum ooo: { red: 0, blue: 1, yellow: 2}
 attr_accessor :ooo
+end
 
