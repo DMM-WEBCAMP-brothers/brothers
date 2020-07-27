69d19302dfb1a9c17f14107289b648e3b03b68ba
vagrant <vagrant@localhost.localdomain>
2020-07-27 08:26:47 +0000

    “[add]enum”

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,12 @@ class Order < ApplicationRecord
 has_many :order_product, dependent: :destroy
 belongs_to :member
 enum payment_method: { "銀行振り込み": false, "クレジットカード": true }
+enum status:{
+    wait:       0, #入金待ち
+    check:      1, #入金確認
+    prepare:    2, #発送準備中
+    complete:   3, #発送済み
+  }
+
 attr_accessor :ooo
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,5 +1,9 @@
 class OrderProduct < ApplicationRecord
 
   belongs_to :order
-
+enum production_status: {
+	 prepare:    0, #制作準備中
+	 doing:      1, #制作途中
+ 	 complete:   2, #制作完了
+}
 end
