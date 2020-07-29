7d0ef7d4d30adf0b90e60eb6599e6658b6ce34bd
vagrant <vagrant@localhost.localdomain>
2020-07-29 08:10:16 +0000

    [Update]status

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,9 +1,11 @@
 class OrderProduct < ApplicationRecord
 enum production_status: {
-	 prepare:    0, #制作準備中
-	 doing:      1, #制作途中
- 	 complete:   2, #制作完了
+	 dont:       0, #着手不可
+	 wait:       1, #制作待ち
+	 doing:      2, #制作中
+ 	 complete:   3, #制作完了
 }
+
 	belongs_to :order
 	belongs_to :product
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_061324) do
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "name"
+    t.string "name"
     t.boolean "is_valid", default: true, null: false
   end
 
